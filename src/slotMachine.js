5714c180083cf051fdefbf45bd4d4686e927df66
Thomas Rutzer <thomas.rutzer@artcom.de>
2021-03-20 21:06:36 +0100

    Hide on start

--- src/slotMachine.js
+++ src/slotMachine.js
@@ -1,8 +1,8 @@
-import React from "react"
-import anime from "animejs"
-import Slot from "./slot"
-import * as utils from "./utils"
-import slotMachineStyles from "./slotMachine.module.scss"
+import React from 'react'
+import anime from 'animejs'
+import Slot from './slot'
+import * as utils from './utils'
+import slotMachineStyles from './slotMachine.module.scss'
 
 export default class SlotMachine extends React.Component {
   constructor(props) {
@@ -28,28 +28,29 @@ export default class SlotMachine extends React.Component {
       targets: this.slotMachineRef.current,
       duration: 1200,
       opacity: 1,
-      easing: "easeInOutQuad",
+      easing: 'easeInOutQuad',
       complete: () => {
         this.spin()
-      },
+      }
     })
   }
 
   render() {
     return (
       <div
-        ref={ this.slotMachineRef }
-        className={ slotMachineStyles.wrapper }>
-        <div className={ slotMachineStyles.slotMachine }>
+        style='opacity: 0'
+        ref={this.slotMachineRef}
+        className={slotMachineStyles.wrapper}>
+        <div className={slotMachineStyles.slotMachine}>
           { this.slots.map((slot, index) =>
             <Slot
-              key={ index }
-              slotIndex={ index }
-              spin={ this.state.isSpinning }
-              evaluate={ (slotIndex, symbol) => this.saveSingleResult(slotIndex, symbol) }
-              symbolDefaultStyleClass={ this.props.symbolDefaultStyleClass }
-              symbolEvaluatedStyleClass={ this.props.symbolEvaluatedStyleClass }
-              symbols={ slot.symbols } />
+              key={index}
+              slotIndex={index}
+              spin={this.state.isSpinning}
+              evaluate={(slotIndex, symbol) => this.saveSingleResult(slotIndex, symbol)}
+              symbolDefaultStyleClass={this.props.symbolDefaultStyleClass}
+              symbolEvaluatedStyleClass={this.props.symbolEvaluatedStyleClass}
+              symbols={slot.symbols} />
           ) }
         </div>
       </div>
@@ -85,11 +86,11 @@ export default class SlotMachine extends React.Component {
       symbols: !initital
         ? [
           ...slot.symbols.slice(slot.symbols.length - 3, slot.symbols.length),
-          ...utils.fillByWithRandomElement(this.props.symbolsPerSlot - 1, this.props.symbols),
+          ...utils.fillByWithRandomElement(this.props.symbolsPerSlot - 1, this.props.symbols)
         ]
         : [
           ...utils.fillByWithRandomElement(this.props.symbolsPerSlot - 1, this.props.symbols),
-          ...slot.symbols.slice(slot.symbols.length - 3, slot.symbols.length),
+          ...slot.symbols.slice(slot.symbols.length - 3, slot.symbols.length)
         ]
     }))
   }
@@ -101,16 +102,16 @@ export default class SlotMachine extends React.Component {
         initialSymbol,
         utils.getRandomElementFromList(this.props.symbols)
       ],
-      evaluation: null,
+      evaluation: null
     }
   }
 }
 
 SlotMachine.defaultProps = {
-  symbols: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-  initialSymbols: ["1", "2", "3"],
+  symbols: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+  initialSymbols: ['1', '2', '3'],
   symbolsPerSlot: 20,
-  symbolDefaultStyleClass: "slotMachine__symbol",
-  symbolEvaluatedStyleClass: "slotMachine__symbol--evaluated",
+  symbolDefaultStyleClass: 'slotMachine__symbol',
+  symbolEvaluatedStyleClass: 'slotMachine__symbol--evaluated',
   onSpinningEnd: () => {}
 }
